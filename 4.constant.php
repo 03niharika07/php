3cd470c681ffd18dd15ad77273498ede06ce9834
03niharika07 <niharika20052004@gmail.com>
2026-06-19 23:08:46 +0530

    New changes

---
+++ 4.constant.php
@@ -0,0 +1,33 @@
+<html>
+    <head><title>PHP</title><head>
+            <body>
+                <center>
+                <?php
+                $a = 100; // variable 
+                $b = 200;
+                $c = $a + $b;
+
+                /*
+                Constant declaration syntax\
+                define(string name,value,case sensitive) 
+                (by default case sensitive value false hoti h mtlb case sensitive hota hai)
+                */
+
+                define("pi",3.14, false);
+                echo pi;   // by default case sensitive hota hai
+                echo "<br>";
+
+                // Another method of declaration
+                const PI = 3.14;
+                echo PI;
+                echo "<br>";
+
+                // Radius of a circle
+                $r = 2;
+                $A = PI*$r*$r;
+                echo "Area of circle with radius $r is $A";
+
+                ?>
+                </center>
+            </body>
+</html>
